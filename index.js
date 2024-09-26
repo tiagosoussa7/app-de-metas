d1c7d19f37667d5f3a127abddef79894c91a8984
tiago <tg.sousa77.t@gmail.com>
2024-09-26 16:55:51 -0300

    adiciona inquirer

---
+++ index.js
@@ -0,0 +1,14 @@
+const start = () => {
+    while (Option) {
+        let option = "out"
+        switch (option) {
+            case "cadastrar":
+                break;
+            case "listar":
+                break;
+            case "sair":
+                break;
+        }
+
+    }
+}
