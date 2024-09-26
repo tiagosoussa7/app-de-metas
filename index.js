985e7ba40c75b0a5a2f60b75370ee62f18a1b39a
tiago <tg.sousa77.t@gmail.com>
2024-09-26 17:16:56 -0300

    iniciando menu

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const start = () => {
-    while (Option) {
-        let option = "out"
-        switch (option) {
-            case "cadastrar":
-                break;
-            case "listar":
-                break;
-            case "sair":
-                break;
-        }
+const { select } = require("@inquirer/prompts");
 
+const start = async () => {
+  while (true) {
+    const option = await select({
+      message: "Menu >",
+      choices: [
+          {
+            name: "Cadastrar meta",
+            value: "cadastrar",
+          },
+          {
+            name: "Listar metas",
+            value: "cadastrar",
+          },
+          {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    });
+    switch (option) {
+      case "cadastrar":
+        console.log("vamos cadastrar");
+
+        break;
+      case "listar":
+        console.log("vamos listar");
+
+        break;
+      case "sair":
+        console.log("até a próxima");
+
+        break;
     }
-}
+  }
+};
+
+start()
