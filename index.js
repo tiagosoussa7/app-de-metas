ec3b794b865aeb6f3c91b896ea584f71a37ed52e
tiago <tg.sousa77.t@gmail.com>
2024-09-26 18:25:00 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-const { select, input } = require("@inquirer/prompts");
-
-let metas = [ meta ]
+const { select, input, checkbox } = require("@inquirer/prompts");
+let meta = {
+  value: "Tomar 3 litros de água por dia.",
+  checked: false,
+};
+let metas = [meta];
 
 const cadastrarMeta = async () => {
-    const meta = await input({ message: "Digite a meta:"})
+  const meta = await input({ message: "Digite a meta:" });
 
-    if(meta.length == 0) { 
-        console.log('A meta não pode estar vazia');
-        return
-    }
+  if (meta.length == 0) {
+    console.log("A meta não pode estar vazia");
+    return;
+  }
 
-    metas.push(
-        { value: meta, checked: false }
-    )
-}
+  metas.push({ value: meta, checked: false });
+};
+
+const listarMetas = async () => {
+  const respostas = await checkbox({
+    message:
+      "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  });
+
+  if (respostas.length == 0) {
+    console.log("Nenhuma meta selecionada.");
+    return;
+  }
+
+  metas.forEach((m) => {
+    m.checked = false
+  })
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+
+    meta.checked = true;
+  });
+  console.log("Meta(s) marcada(s) como concluída(s).");
+};
+
 const start = async () => {
   while (true) {
     const option = await select({
       message: "Menu >",
       choices: [
-          {
-            name: "Cadastrar meta",
-            value: "cadastrar",
-          },
-          {
-            name: "Listar metas",
-            value: "cadastrar",
-          },
-          {
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listar metas",
+          value: "listar",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -36,19 +65,19 @@ const start = async () => {
 
     switch (option) {
       case "cadastrar":
-        await cadastrarMeta()
+        await cadastrarMeta();
 
         break;
       case "listar":
-        console.log("vamos listar");
+        await listarMetas();
 
         break;
       case "sair":
-        console.log("até a próxima");
+        await sair();
 
         break;
     }
   }
 };
 
-start()
+start();
