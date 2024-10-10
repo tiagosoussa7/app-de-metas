15cc201bfba5e196384d4b0f97b01e516360101e
tiago <tg.sousa77.t@gmail.com>
2024-10-10 15:40:30 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const listarMetas = async () => {
   }
 
   metas.forEach((m) => {
-    m.checked = false
-  })
+    m.checked = false;
+  });
 
   respostas.forEach((resposta) => {
     const meta = metas.find((m) => {
@@ -41,6 +41,21 @@ const listarMetas = async () => {
     meta.checked = true;
   });
   console.log("Meta(s) marcada(s) como concluída(s).");
+};
+
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+    return meta.checked;
+  });
+
+  if (realizadas.length == 0) {
+    console.log("Não existem metas realizadas!");
+    return;
+  }
+  await select({
+    message: "Metas realizadas",
+    choices: [...realizadas],
+  });
 };
 
 const start = async () => {
@@ -57,6 +72,10 @@ const start = async () => {
           value: "listar",
         },
         {
+          name: "Metas realizadas",
+          value: "realizadas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -66,15 +85,17 @@ const start = async () => {
     switch (option) {
       case "cadastrar":
         await cadastrarMeta();
-
+        console.log("Metas");
         break;
       case "listar":
         await listarMetas();
-
+        break;
+      case "realizadas":
+        await metasRealizadas();
         break;
       case "sair":
         await sair();
-
+        console.log("Até a próxima");
         break;
     }
   }
